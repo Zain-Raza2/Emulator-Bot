365b0a3a72a97a020ae470009d045a6b49cf69b6
unknown <rbxzain@gmail.com>
2021-10-20 09:29:55 +0100

    Created Client, Global Var and init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { Client, Collection } from 'discord.js'
+
+// Client
+
+const client = new Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MEMBERS",
+        "GUILDS_EMOJIS",
+        "GUILD_MESSAGES",
+        "GUILD_INVITES",
+        "GUILD_VOICE_STATES",
+    ]
+})
+
+module.exports = client
+
+// Global Variables 
+
+client.commands = new Collection()
+client.config = require("../config.json")
+client.slashCommands = new Collection()
+
+// Initialion
+
+require("./handler")(client)
+
+client.login(client.config.token)
